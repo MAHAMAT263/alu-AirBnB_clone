09c2fed555064fc50d37568662cc0ed09ede3ad9
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-16 10:50:20 -0800

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 import json
-from datetime import datetime
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
         serialized_objects = {}
         for key, obj in self.__objects.items():
             serialized_objects[key] = obj.to_dict()
@@ -22,15 +24,15 @@ class FileStorage:
             json.dump(serialized_objects, file)
 
     def reload(self):
+        """Deserializes the JSON file to __objects."""
         try:
             with open(self.__file_path, 'r') as file:
                 data = json.load(file)
 
+            from models.base_model import BaseModel  # Import inside the method to avoid circular imports
             for key, value in data.items():
                 class_name, obj_id = key.split('.')
-                # Use globals() to get the class by its name as a string
-                obj_class = globals()[class_name]
-                obj = obj_class(**value)
+                obj = eval(class_name)(**value)
                 self.__objects[key] = obj
 
         except FileNotFoundError:
