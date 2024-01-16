73acee4296bc51503174f2067e08946356e488b3
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-16 09:07:18 -0800

    Airbnb

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
-
 from models.base_model import BaseModel
 
-if __name__ == "__main__":
-    bm = BaseModel()
+# Test BaseModel: to_dict()
+bm1 = BaseModel()
+d_json = bm1.to_dict()
+assert type(d_json['created_at']) == str
+assert type(d_json['updated_at']) == str
+assert type(d_json['__class__']) == str
+assert type(d_json) == dict
 
-    # Test the to_dict method
-    d_json = bm.to_dict()
-    print(type(bm.created_at))
-    print(type(d_json['created_at']))
-    print(type(d_json['updated_at']))
-    print(type(d_json['__class__']))
+print("OK")
+
+# Test BaseModel: self.id
+bm2 = BaseModel()
+assert type(bm2.id) == str
+
+print("OK")
+
+# Test BaseModel: self.created_at
+bm3 = BaseModel()
+assert type(bm3.created_at) == datetime
+
+print("OK")
+
+# Test BaseModel: __str__(self)
+bm4 = BaseModel()
+str_representation = str(bm4)
+assert type(str_representation) == str
+
+print("OK")
