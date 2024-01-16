7dcee86ebfbe46f453f09f7243037295b897c2fe
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-16 10:25:56 -0800

    Airbnb

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 # Test FileStorage: __file_path
 storage = FileStorage()
