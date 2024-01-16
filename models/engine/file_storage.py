e62cd2e13c5a6750c533ca0adfbd5b72ed84266d
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-16 10:32:43 -0800

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-import uuid
-from datetime import datetime
 import json
+from models.base_model import BaseModel  # Ensure correct import statement
+from datetime import datetime
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
         serialized_objects = {}
         for key, obj in self.__objects.items():
             serialized_objects[key] = obj.to_dict()
@@ -26,12 +23,10 @@ class FileStorage:
             json.dump(serialized_objects, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
         try:
             with open(self.__file_path, 'r') as file:
                 data = json.load(file)
 
-            from models.base_model import BaseModel  # Import inside the method to avoid circular imports
             for key, value in data.items():
                 class_name, obj_id = key.split('.')
                 obj = eval(class_name)(**value)
