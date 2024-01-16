d5536bb0873246283951fb927712290c3dd1faaa
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-16 09:29:40 -0800

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,20 @@ from datetime import datetime
 #Write a class BaseModel that defines all common attributes/methods for other classes
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+    def __init__(self, *args, **kwargs):
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     def save(self):
         self.updated_at = datetime.now()
     def to_dict(self):
