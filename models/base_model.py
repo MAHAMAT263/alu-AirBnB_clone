7b53c0afda95d81ec039f47304228b9c94813be3
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-16 09:41:30 -0800

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+from models import storage
 import uuid
 from datetime import datetime
-#Write a class BaseModel that defines all common attributes/methods for other classes
 
 class BaseModel:
-
     def __init__(self, *args, **kwargs):
-
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -18,8 +16,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
+
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
+
     def to_dict(self):
         class_name = self.__class__.__name__
         dict_representation = self.__dict__.copy()
@@ -27,6 +29,7 @@ class BaseModel:
         dict_representation['created_at'] = self.created_at.isoformat()
         dict_representation['updated_at'] = self.updated_at.isoformat()
         return dict_representation
+
     def __str__(self):
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects."""
+        try:
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+
+            from models.base_model import BaseModel  # Import inside the method to avoid circular imports
+            for key, value in data.items():
+                class_name, obj_id = key.split('.')
+                obj = eval(class_name)(**value)
+                self.__objects[key] = obj
+
+        except FileNotFoundError:
+            pass
