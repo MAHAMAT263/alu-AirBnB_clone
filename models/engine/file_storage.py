1c9ec5b53416abf0103001a43587e5fb07fcc617
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-18 06:29:36 -0800

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,8 +4,8 @@ import os
 from models.base_model import BaseModel
 
 class FileStorage:
-    __file_path = "file.json"
-    __objects = {}
+    __file_path: str = "file.json"
+    __objects: dict = {}
 
     def all(self):
         """Returns the dictionary __objects."""
@@ -13,31 +13,28 @@ class FileStorage:
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
         serialized_objects = {}
         for key, obj in self.__objects.items():
-            serialized_objects[key] = obj.to_dict()
-
+        serialized_objects[key] = obj.to_dict()
         with open(self.__file_path, 'w') as file:
-            json.dump(serialized_objects, file)
-
+        json.dump(serialized_objects, file)
+    
     def reload(self):
     """
-    Deserializes the JSON file to __objects
-    (only if the JSON file (__file_path) exists; otherwise, do nothing).
-    If the file doesn’t exist, no exception should be raised.
+    Public instance method:
+    Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists;
+    otherwise, do nothing. If the file doesn’t exist, no exception should be raised.
     """
-    if os.path.exists(self.__file_path):
+    if exists(self.__file_path):
         with open(self.__file_path, 'r') as file:
-            data = json.load(file)
-            for key, obj_dict in data.items():
-                class_name, obj_id = key.split('.')
-                module = import_module('models.' + class_name)
-                class_ = getattr(module, class_name)
-                # Create an instance from the dictionary representation
-                obj = class_.from_dict(obj_dict)
-                self.__objects[key] = obj
+            serialized_objects = json.load(file)
+
+        for key, obj_dict in serialized_objects.items():
+            class_name, obj_id = key.split('.')
+            obj_class = globals()[class_name]
+            obj_instance = obj_class(**obj_dict)
+            self.__objects[key] = obj_instance
