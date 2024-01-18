970675927b1f69d0cb7024a16c2b85ea63f2ca17
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-18 08:01:09 -0800

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -14,8 +14,8 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
