bea9f74ca3bad3220b69c704d74c6b242bc6defc
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-18 07:01:16 -0800

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# models/base_model.py
 import uuid
 from datetime import datetime
 
@@ -21,34 +20,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-
-if __name__ == "__main__":
-    instance = BaseModel()
-    print(type(instance.id)
-    instance = BaseModel()
-    print(type(instance.created_at))
-    instance = BaseModel()
-    print(type(instance.updated_at))
-
-    # 2 instances creation + id different
-    instance1 = BaseModel()
-    instance2 = BaseModel()
-    print(instance1.id != instance2.id)  # Expected: True
-
-    
-    instance = BaseModel()
-    print(str(instance))  # Expected: [BaseModel] (<instance.id>) <instance.__dict__>
-
-    instance = BaseModel()
-    print(type(instance.to_dict()))  # Expected: <class 'dict'>
-    print(type(instance.to_dict()['created_at']))  # Expected: <class 'str'>
-    print(type(instance.to_dict()['updated_at']))  # Expected: <class 'str'>
-    print(type(instance.to_dict()['__class__']))  # Expected: <class 'str'>
-    print(instance.to_dict()['__class__'])  # Expected: 'BaseModel'
-
-    instance = BaseModel()
-    instance.save()
-    print(type(instance.updated_at))  # Expected: <class 'datetime.datetime'>
-    print(type(instance.to_dict()['updated_at']))  # Expected: <class 'str'>
-
