e7bd6cfb182cf6882c227945b2b3d690b0395129
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-18 05:17:44 -0800

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python3
 import json
-from os import path
-from models.base_model import BaseModel
+import os
 
 class FileStorage:
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
         serialized_objects = {}
         for key, obj in self.__objects.items():
             serialized_objects[key] = obj.to_dict()
-        
-        with open(self.__file_path, 'w', encoding='utf-8') as file:
+
+        with open(self.__file_path, 'w') as file:
             json.dump(serialized_objects, file)
 
     def reload(self):
-        if path.exists(self.__file_path):
-            with open(self.__file_path, 'r', encoding='utf-8') as file:
-                serialized_objects = json.load(file)
-
-                for key, value in serialized_objects.items():
+        """
+        Deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists; otherwise, do nothing).
+        If the file doesn’t exist, no exception should be raised.
+        """
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, obj_dict in data.items():
                     class_name, obj_id = key.split('.')
-                    if class_name == 'BaseModel':
-                        class_obj = BaseModel
-                    # Add similar conditions for other classes if needed
-                    else:
-                        raise ValueError(f"Unknown class: {class_name}")
-
-                    self.__objects[key] = class_obj(**value)
+                    # Assuming you have a method from_dict in your classes to create an instance from a dictionary
+                    obj = globals()[class_name].from_dict(obj_dict)
+                    self.__objects[key] = obj
