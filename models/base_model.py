623933c072749a39cc3ef73d57510a335f2afda2
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-18 07:11:01 -0800

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models.__init__ import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -14,12 +15,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         obj_dict = {'__class__': self.__class__.__name__}
