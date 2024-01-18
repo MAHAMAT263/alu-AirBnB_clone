1d4554b416799ead912934c0a193ced8a959ac11
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-18 07:05:16 -0800

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,17 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -15,8 +22,10 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict = {'__class__': self.__class__.__name__}
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                obj_dict[key] = value.isoformat()
+            else:
+                obj_dict[key] = value
         return obj_dict
