1d72b42d70c08cc685b42a3df2bbf923e67a7649
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-19 05:31:05 -0800

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,15 +25,14 @@ class FileStorage:
             json.dump(data, file)
 
     def reload(self):
-        """Deserializes the JSON file (__file_path) to __objects."""
         try:
             with open(self.__file_path, 'r') as file:
                 data = json.load(file)
                 for item_key, item_value in data.items():
                     class_name = item_value['__class__']
-                    # Use globals() to access the class by name
                     model_class = globals()[class_name]
                     instance = model_class(**item_value)
                     self.new(instance)
         except FileNotFoundError:
-            pass     
+            with open(self.__file_path, 'w') as file:
+                file.write("{}")     
